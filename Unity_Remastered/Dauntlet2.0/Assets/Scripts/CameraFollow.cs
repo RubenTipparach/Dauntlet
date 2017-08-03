74db99c43c91efd4705c8dfaa60e54eca1b1b10d
Rubensaccnt <santipab.stephane@gmail.com>
2017-08-03 12:07:23 -0500

    updated version of dante and level stuff!

---
+++ Unity_Remastered/Dauntlet2.0/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    [SerializeField]
+    Transform trackedObj;
+
+    [SerializeField]
+    float easeDistance = 3;
+
+	
+    // Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        Vector2 campos = transform.position;
+        Vector2 targetPos = trackedObj.position;
+
+        if(Vector2.Distance(campos, targetPos) > easeDistance)
+        {
+            transform.position = Vector3.Lerp(transform.position, trackedObj.position + Vector3.forward * transform.position.z, Time.deltaTime );
+        }
+		
+	}
+
+
+}
---
+++ Unity_Remastered/Dauntlet2.0/Assets/Scripts/DantePlayer.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DantePlayer : MonoBehaviour {
+
+    [SerializeField]
+    float moveSpeed = 10;
+
+    Vector3 position;
+
+    Vector3 lastPosition;
+
+    [SerializeField]
+    LayerMask wallLayers;
+
+
+    // Use this for initialization
+    void Start () {
+        position = transform.position;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+
+    private void FixedUpdate()
+    {
+        //lastPosition = transform.position;
+
+        float horiz = Input.GetAxis("Horizontal");
+        float vert = Input.GetAxis("Vertical");
+
+        if (horiz != 0 || vert != 0)
+        {
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(horiz, vert) * moveSpeed, ForceMode2D.Impulse);
+        }
+    }
+
+}
